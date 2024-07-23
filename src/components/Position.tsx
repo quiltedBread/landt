bf488dc29209ae9aff72db66ad7371d51c57fdbc
Matt Harper <quiltedbread@gmail.com>
2024-07-22 22:28:05 -0400

    Added antd tables w/ tags

--- src/components/Position.tsx
+++ src/components/Position.tsx
@@ -1,23 +1,55 @@
+import { Table, Tag } from "antd";
+import type { TableProps } from "antd";
+
 import { Position as PositionProps } from "../types";
-import { Bullet as BulletProps } from '../types';
+import { Bullet as BulletProps } from "../types";
 
+// type TableRowSelection<T> = TableProps<T>["rowSelection"];
 
-function Bullet({props}: {props: BulletProps}) {
-    return (
-        <li>
-            <input type="checkbox" checked={props.selected} />
-            <span>{props.text}</span>
-        </li>
-    );
-}
+const columns: TableProps<BulletProps>["columns"] = [
+    {
+        title: "Text",
+        dataIndex: "text",
+        key: "text",
+    },
+    {
+        title: "Tags",
+        key: "tags",
+        dataIndex: "tags",
+        render: (_, { tags }) => (
+            <>
+                {tags.map((tag) => {
+                    let color = tag.length > 5 ? "geekblue" : "green";
+                    if (tag === "loser") {
+                        color = "volcano";
+                    }
+                    return (
+                        <Tag color={color} key={tag}>
+                            {tag.toUpperCase()}
+                        </Tag>
+                    );
+                })}
+            </>
+        ),
+    },
+];
 
-export default function Position({props}: {props: PositionProps}) {
+// const rowSelection: TableRowSelection<BulletProps> = {
+//     selectedRowKeys,
+//     onChange: onSelectChange,
+// };
+
+export default function Position({ props }: { props: PositionProps }) {
     return (
         <div className="position">
             <h2>{props.title}</h2>
-            <ul>
-                {props.bullets.map(bullet => <Bullet props={bullet}/>)}
-            </ul>
+            <Table
+                columns={columns}
+                dataSource={props.bullets}
+                pagination={false}
+                showHeader={false}
+                // rowSelection={}
+            />
         </div>
     );
 }
